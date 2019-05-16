6709ead312c681bb6ec9d470617c90df4f6acb40
huangtianning <907442024@qq.com>
2019-05-16 22:51:52 +0800

    update

---
+++ src/test/java/com/whut/www/service/Test.java
@@ -0,0 +1,33 @@
+/**  
+* @Title: Test.java  
+* @Package com.whut.www.service  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author HuangTianning
+* @date 2019年5月9日  
+* @version V1.0  
+*/  
+
+package com.whut.www.service;
+
+/**  
+* @ClassName: Test  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author HuangTianning
+* @date 2019年5月9日  
+*    
+*/
+public class Test {
+
+	/**  
+	* @Title: main  
+	* @Description: TODO(这里用一句话描述这个方法的作用)  
+	* @param args  void 返回类型
+	* @throws：异常信息
+	*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdaf");
+	}
+
+}
